7fd5cedd4c49a2415a6f2191b9ff9c10f6d5b512
john-jaihyek-choi <jaihyek.choi@gmail.com>
2025-03-28 00:08:07 -0500

    interpreter debugged, tailwind debugged,

---
+++ babel.config.js
@@ -0,0 +1,11 @@
+// babel.config.js
+module.exports = {
+  presets: [
+    [
+      "@babel/preset-react",
+      {
+        runtime: "automatic",
+      },
+    ],
+  ],
+};
---
+++ src/pages/projects/index.tsx
@@ -0,0 +1,4 @@
+const index = () => {
+  return <div>projects</div>;
+};
+export default index;
