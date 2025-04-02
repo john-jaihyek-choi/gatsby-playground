1a910023e0ad74e77d9336efb47a5d560f604f9c
john-jaihyek-choi <jaihyek.choi@gmail.com>
2025-04-01 22:58:56 -0500

    reviewed plug-in concept, basic graphQL query concept, and static/dynamic image render with image optimization

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,6 +2,12 @@ import * as React from "react";
 import type { PageProps } from "gatsby";
 import Layout from "../components/Layouts";
 import { graphql } from "gatsby";
+import {
+  StaticImage,
+  getImage,
+  IGatsbyImageData,
+  GatsbyImage,
+} from "gatsby-plugin-image";
 
 export const query = graphql`
   query SiteInfo {
@@ -11,8 +17,21 @@ export const query = graphql`
         title
       }
     }
+    images: allFile(filter: { sourceInstanceName: { eq: "images" } }) {
+      nodes {
+        childImageSharp {
+          gatsbyImageData(width: 500)
+        }
+      }
+    }
   }
 `;
+
+interface ImageNode {
+  childImageSharp: {
+    gatsbyImageData: IGatsbyImageData;
+  };
+}
 
 interface DataType {
   site: {
@@ -21,6 +40,10 @@ interface DataType {
       title: string;
     };
   };
+  images: {
+    nodes: ImageNode[];
+  };
+  extensions: {};
 }
 
 interface HomeProps extends PageProps {
@@ -36,7 +59,19 @@ const Home: React.FC<HomeProps> = ({ data, path }: HomeProps) => {
       <p>Pikachu</p>
       <p>{title}</p>
       <p>{description}</p>
-      <img src="/pikachu.png" alt="pikachu.png" />
+      {data.images.nodes.map((imgfile, index) => {
+        const img = getImage(imgfile.childImageSharp);
+
+        if (!img) return null;
+
+        return (
+          <GatsbyImage
+            key={index}
+            image={img!}
+            alt={`imgfile-${index}`}
+          ></GatsbyImage>
+        );
+      })}
     </Layout>
   );
 };
